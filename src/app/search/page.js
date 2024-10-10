c6060736cd8eac3d87a365ce2a411a1ee7c2655e
Jesutobi <jesutobi_dev@outlook.com>
2024-10-10 13:21:36 +0100

    client-side static rendering fix

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchMovies, incrementPage } from '../redux/slices/movieSlice';
 import { useSearchParams } from 'next/navigation';
@@ -38,58 +38,60 @@ const SearchPage = () => {
     }
 
     return (
-        <div className="px-5 lg:px-10 py-5 lg:py-10">
-            <Header />
-            {
-                searchQuery ? (
-                    searchStatus === 'succeeded' && searchResults.length > 0 ? (
-                        <>
-                            <div className="mt-20 divide-y divide-black">
-                                {searchResults.map(movie => (
-                                    <Link key={movie.id} href={`/${movie.id}`} className="flex gap-2 py-4 w-full">
-                                        <div className="w-32 h-32 relative rounded-lg overflow-hidden">
-                                            <Image
-                                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                                                alt={movie.title}
-                                                fill
-                                                priority
-                                                className="object-cover"
-                                            />
-                                        </div>
-                                        <p className="font-medium text-base md:text-lg lg:text-2xl flex-1">{movie.title}</p>
-                                    </Link>
-                                ))}
-                            </div>
+        <Suspense fallback={<Loading />}>
+            <div className="px-5 lg:px-10 py-5 lg:py-10">
+                <Header />
+                {
+                    searchQuery ? (
+                        searchStatus === 'succeeded' && searchResults.length > 0 ? (
+                            <>
+                                <div className="mt-20 divide-y divide-black">
+                                    {searchResults.map(movie => (
+                                        <Link key={movie.id} href={`/${movie.id}`} className="flex gap-2 py-4 w-full">
+                                            <div className="w-32 h-32 relative rounded-lg overflow-hidden">
+                                                <Image
+                                                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                                    alt={movie.title}
+                                                    fill
+                                                    priority
+                                                    className="object-cover"
+                                                />
+                                            </div>
+                                            <p className="font-medium text-base md:text-lg lg:text-2xl flex-1">{movie.title}</p>
+                                        </Link>
+                                    ))}
+                                </div>
 
-                            {/* Pagination */}
-                            <div className="flex items-center justify-center gap-5 mt-5">
-                                <button
-                                    disabled={currentPage === 1}
-                                    onClick={handlePrevPage}
-                                    className={`px-4 py-2 border border-black ${currentPage === 1 ? ' cursor-not-allowed' : 'text-black'}`}
-                                >
-                                    <PiCaretLeftBold className="text-black text-2xl" />
-                                </button>
-                                <p className="text-black text-2xl">{currentPage}</p>
-                                <button
-                                    disabled={currentPage >= totalPages}
-                                    onClick={handleNextPage}
-                                    className="px-4 py-2 border border-black text-black"
-                                >
-                                    <PiCaretRightBold className="text-black text-2xl" />
-                                </button>
-                            </div>
-                        </>
+                                {/* Pagination */}
+                                <div className="flex items-center justify-center gap-5 mt-5">
+                                    <button
+                                        disabled={currentPage === 1}
+                                        onClick={handlePrevPage}
+                                        className={`px-4 py-2 border border-black ${currentPage === 1 ? ' cursor-not-allowed' : 'text-black'}`}
+                                    >
+                                        <PiCaretLeftBold className="text-black text-2xl" />
+                                    </button>
+                                    <p className="text-black text-2xl">{currentPage}</p>
+                                    <button
+                                        disabled={currentPage >= totalPages}
+                                        onClick={handleNextPage}
+                                        className="px-4 py-2 border border-black text-black"
+                                    >
+                                        <PiCaretRightBold className="text-black text-2xl" />
+                                    </button>
+                                </div>
+                            </>
+                        ) : (
+                            <p className="text-center text-xl w-screen h-screen flex items-center justify-center">Seems we couldn&apos;t find anything</p>
+                        )
                     ) : (
-                        <p className="text-center text-xl w-screen h-screen flex items-center justify-center">Seems we couldn&apos;t find anything</p>
+                        <p className="text-center text-2xl opacity-50 italic w-screen h-screen flex items-center justify-center">
+                            Search to see movies.
+                        </p>
                     )
-                ) : (
-                    <p className="text-center text-2xl opacity-50 italic w-screen h-screen flex items-center justify-center">
-                        Search to see movies.
-                    </p>
-                )
-            }
-        </div>
+                }
+            </div>
+        </Suspense>
     );
 };
 
